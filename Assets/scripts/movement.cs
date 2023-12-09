1570b010f1fb66d57ab98bcb7fde33a7af22f3f3
EneMatei <ene.matei21@gmail.com>
2023-12-09 14:16:49 +0200

    start

--- Assets/scripts/movement.cs
+++ Assets/scripts/movement.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class movement : MonoBehaviour
 {
+    private Rigidbody2D body;
+    [SerializeField] private float speed;
+
     // Start is called before the first frame update
-    void Start()
+    void Start()    
     {
-        
+        body = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
-    {
-        
+    { 
+          body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
     }
 }
