72e45dca0f597c329b7b3f644b7a608f7cb50bce
xsop <40323764+xsop@users.noreply.github.com>
2023-12-10 02:05:23 +0200

    health bars
    poggers

--- Assets/scripts/TakeDamage.cs
+++ Assets/scripts/TakeDamage.cs
@@ -1,25 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TakeDamage : MonoBehaviour
-{ 
+{
     public Animator animator;
+    public Slider healthSlider; // Reference to the UI Slider
 
     private float maxHealth = 100;
     private float currentHealth;
 
-    // Start is called before the first frame update
     void Start()
     {
         currentHealth = maxHealth;
+
+        // Set the initial value of the health slider
+        if (healthSlider != null)
+        {
+            healthSlider.maxValue = maxHealth;
+            healthSlider.value = currentHealth;
+        }
     }
 
     public void OnTakeDamage(float damage)
     {
         currentHealth -= damage;
-
-        // Invoke("DeployAnimation", 0.6f);
+        UpdateHealthUI(); // Update the health bar
 
         DeployAnimation();
 
@@ -37,5 +44,24 @@ public class TakeDamage : MonoBehaviour
     void Die()
     {
         animator.SetBool("IsDead", true);
+    }
+
+    void UpdateHealthUI()
+    {
+        // Update the health bar value
+        if (healthSlider != null)
+        {
+            healthSlider.value = currentHealth;
+
+            // Check if health is zero, and make the fill invisible
+            if (currentHealth <= 0)
+            {
+                Image fillImage = healthSlider.fillRect.GetComponent<Image>();
+                if (fillImage != null)
+                {
+                    fillImage.color = new Color(0, 0, 0, 0); // Set alpha to 0 (completely transparent)
+                }
+            }
+        }
     }
 }
